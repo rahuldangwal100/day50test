f2197f2ee5af0f9ca32160cb9813f32d0198b041
rahuldangwal100 <rahul.sd200@gmail.com>
2020-12-04 23:12:33 +0530

    second

---
+++ WebApplD50Test/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplD50Test.Models
+{
+    public class Student
+    {
+        public int SId { get; set; }
+        public string SName { get; set; }
+        public string SDob { get; set; }
+
+        public string SContact { get; set; }
+    }
+}
